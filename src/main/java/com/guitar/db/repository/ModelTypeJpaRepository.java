af7f65dfc1f88a39be00327ecac40ecd0cd27cb8
eight9080 <eight9080@gmail.com>
2018-03-12 23:58:55 +0200

    spring data jpa repository

--- src/main/java/com/guitar/db/repository/ModelTypeJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelTypeJpaRepository.java
@@ -1,11 +1,13 @@
 package com.guitar.db.repository;
 
-import com.guitar.db.model.Location;
+import com.guitar.db.model.ModelType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocationJpaRepository extends JpaRepository<Location, Long>{
+public interface ModelTypeJpaRepository extends JpaRepository<ModelType, Long>{
+
+
 
 
 }

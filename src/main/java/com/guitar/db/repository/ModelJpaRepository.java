5e5f0c0cc924042b8f8a24cc5a40d2b4b155d891
eight9080 <eight9080@gmail.com>
2018-03-16 00:22:02 +0200

    spring data query DSL

--- src/main/java/com/guitar/db/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelJpaRepository.java
@@ -4,8 +4,15 @@ import com.guitar.db.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface ModelJpaRepository  extends JpaRepository<Model, Long>{
+
+
+    List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal lowest, BigDecimal highest);
+//    List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal low, BigDecimal high);
 
 
 }
